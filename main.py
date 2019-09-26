1f9f4e3570deef83894db5263308289e2842045e
MORERA Georges <ce_morge@eolane.com>
2019-09-26 17:54:20 +0200

    ajout des déplacements de mac_gyver. le jeu freeze.

--- main.py
+++ main.py
@@ -5,7 +5,37 @@ from labyrinths import Labyrinth
 from constants import LAB, FLOOR, MAC, KEEP, NEEDLE, TUBE, CHLOROFORME, PIXEL_ROW, PIXEL_COLUMN
 
 class Engine:
-    def init(self):
+    def main(self):
+        self.labyrinth = Labyrinth(LAB)
+        self.mg = Character()
+
+        self.display()
+        end = 0
+        while True:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                self.displace('UP')
+            elif keys[pygame.K_DOWN]:
+                self.displace('DOWN')
+            elif keys[pygame.K_LEFT]:
+                self.displace('LEFT')
+            elif keys[pygame.K_RIGHT]:
+                self.displace('RIGHT')
+            elif keys[pygame.K_ESCAPE]:
+                end = 1
+
+            if end == 1:
+                break
+        
+        self.credit()
+
+
+    def credit(self):
+        print('Dévelopement: Georges Morera \nUn grand merci à OpenClassrooms pour le défi et les images. \nMerci d avoir joué!')
+
+
+    def display(self):
+        """"affiche le jeu."""
         self.backgrounds = pygame.image.load(FLOOR)
         self.mac_pic = pygame.image.load(MAC)
         self.keep_pic = pygame.image.load(KEEP)
@@ -13,64 +43,50 @@ class Engine:
         self.tube_pic = pygame.image.load(TUBE)
         self.chlor_pic = pygame.image.load(CHLOROFORME)
 
-
-    def main(self):
-        lab = Labyrinth(LAB)
-        MAC_GYVER = Character()
-
-        self.display(MAC_GYVER, lab)
-
-        new_position = [0,0]
-        end = 0
-        while True:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_UP]:
-                new_position = [MAC_GYVER.position[0]-1, MAC_GYVER.position[1]]
-                end = MAC_GYVER.moving(new_position, lab)
-            elif keys[pygame.K_DOWN]:
-                new_position = [MAC_GYVER.position[0]+1, MAC_GYVER.position[1]]
-                end = MAC_GYVER.moving(new_position, lab)
-            elif keys[pygame.K_LEFT]:
-                new_position = [MAC_GYVER.position[0], MAC_GYVER.position[1]-1]
-                end = MAC_GYVER.moving(new_position, lab)
-            elif keys[pygame.K_RIGHT]:
-                new_position = [MAC_GYVER.position[0], MAC_GYVER.position[1]+1]
-                end = MAC_GYVER.moving(new_position, lab)
-            elif keys[pygame.K_ESCAPE]:
-                end == 1
-            
-            if end == 1:
-                break
-        
-        self.credit()
-
-    def credit(self):
-        print('Dévelopement: Georges Morera \nUn grand merci à OpenClassrooms pour le défi et les images. \nMerci d avoir joué!')
-    
-    def display(self, hero, lab):
-        """"affiche le jeu."""
         # Charge le fond du labyrinthe et place Mac Gyver et le gardien
-        window = pygame.display.set_mode((lab.row_length*PIXEL_ROW,
-                                          lab.column_length*PIXEL_COLUMN))
-        for l, line in enumerate(lab.lab):
-            for c, s in enumerate(line):
-                pos = (l, c)
+        self.window = pygame.display.set_mode((self.labyrinth.row_length*PIXEL_ROW,
+                                          self.labyrinth.column_length*PIXEL_COLUMN))
+        for c, line in enumerate(self.labyrinth.lab):
+            for l, s in enumerate(line):
+                pos = (l*PIXEL_ROW, c*PIXEL_COLUMN)
                 if s == '0':
-                    window.blit(self.backgrounds, pos, (0,40,20,20))
+                    self.window.blit(self.backgrounds, pos, (0,40,20,20))
                 elif s == '1':
-                    window.blit(self.backgrounds, pos, (100,320,20,20))
+                    self.window.blit(self.backgrounds, pos, (100,320,20,20))
                 elif s == 'K':
-                    window.blit(self.mac_pic, pos)
+                    self.window.blit(self.mac_pic, pos)
                 elif s == 'M':
-                    hero.position = pos
-                    window.blit(self.keep_pic, pos)
+                    self.mg.position = (l, c)
+                    self.window.blit(self.keep_pic, pos)
                 elif s == 'n':
-                    window.blit(self.need_pic, pos)
+                    self.window.blit(self.need_pic, pos)
                 elif s == 't':
-                    window.blit(self.tube_pic, pos)
+                    self.window.blit(self.tube_pic, pos)
                 elif s == 'c':
-                    window.blit(self.chlor_pic, pos)
+                    self.window.blit(self.chlor_pic, pos)
         pygame.display.flip()
+
+    
+    def displace(self, move):
+        """Déplace mac_gyver d'une case."""
+        pos = (self.mg.position[0]*PIXEL_ROW, self.mg.position[1]*PIXEL_COLUMN)
+        self.window.blit(self.backgrounds, pos, (0,40,20,20))
+        if move == 'UP':
+            new_position = (self.mg.position[0]-1, self.mg.position[1])
+        elif move == 'DOWN':
+            new_position = (self.mg.position[0]+1, self.mg.position[1])
+        elif move == 'LEFT':
+            new_position = (self.mg.position[0], self.mg.position[1]-1)
+        elif move == 'RIGHT':
+            new_position = (self.mg.position[0], self.mg.position[1]+1)
+        
+        end = self.mg.moving(new_position, self.labyrinth.lab)
+        pos = (self.mg.position[0]*PIXEL_ROW, self.mg.position[1]*PIXEL_COLUMN)
+        self.window.blit(self.mac_pic, pos)
+        pygame.display.flip()
+        return end
+
+
     
 
 if __name__ == "__main__":
